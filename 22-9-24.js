f4c5fb9f2c6b983cfe41ed0033ff5274e83a1fab
WebDevByAwaisRafiq <webdevbyawais@gmail.com>
2024-09-26 22:33:04 +0500

    Add files via upload

---
+++ 22-9-24.js
@@ -0,0 +1,43 @@
+// let myArray = [ {name : 'awais' ,class : "bscs"} ,
+//                 {name : 'ahmed' ,class : "BBA"}  ,
+//                 {name : 'dawud' ,class : "BSSE"}
+//               ]
+
+// let requiredStudent;
+// console.log("BEFORE : " , requiredStudent);
+
+// myArray.map((item)=>{      if (item.name === "ahmed") {  requiredStudent = item.name      }})
+//     console.log("AFTER : " , requiredStudent);
+
+
+
+//  9- 26-24
+
+// for ( let i = 1 ; i <= 10 ; i++){
+
+// console.log("5 * ",i , " = " , i*5 );
+
+// }
+
+
+// for (let i = 1; i <= 3; i++) {
+
+//     console.log("i value", i);
+
+//     for (let j = 1; j <= 3; j++) {
+
+//         j != i && console.log("j value", j);
+
+
+//     }
+
+
+
+// }
+
+// let  value = ''
+
+// value && console.log("not null");
+let user  = '';
+
+(user )&& console.log("not user");
---
+++ index.js
@@ -0,0 +1,16 @@
+// let n=5;
+// let i;
+// for(let i=1;i<5;i++){
+//     console.log(`${n} * ${i} = ${n * i} `);
+// }
+// using one variable
+// let num=5;
+// for(let i=1;i<=10;i++){
+//     console.log(`${num} * ${i} = ${num * i} `);
+// }
+// using two variable
+let n=5;
+let i;
+for( i=1;i<=10;i++){
+    console.log(`${n} * ${i} = ${n * i} `);
+}
